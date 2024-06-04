9584a7058b34ef6fdb869377328a662c1dd0dbe8
QianjunL <qianjunliang.ql@gmail.com>
2024-06-03 22:53:02 -0600

    update mobile design

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,40 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./sidebar.scss";
-import ToggleButton from './toggleButton/ToggleButton';
-import Links from './links/Links';
-import { motion } from 'framer-motion';
+import ToggleButton from "./toggleButton/ToggleButton";
+import Links from "./links/Links";
+import { motion } from "framer-motion";
 
 const variants = {
-    open: {
-        clipPath: "circle(1200px at 50px 50px)",
-        transition: {
-            type: "spring",
-            stiffness: 20,
-            
-        }
+  open: {
+    clipPath: "circle(1200px at 50px 50px)",
+    transition: {
+      type: "spring",
+      stiffness: 20,
     },
-    closed: {
-        clipPath: "circle(30px at 50px 50px)",
-        transition: {
-            delay: 0.5,
-            type: "spring",
-            stiffness: 400,
-            damping: 40,
-        },
+  },
+  closed: {
+    clipPath: "circle(30px at 50px 50px)",
+    transition: {
+      delay: 0.5,
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
     },
+  },
 };
 
 const Sidebar = () => {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    return (
-        <motion.div className="sidebar" animate={open ? "open" : "closed"}>
-            <motion.div className="bg" variants={variants}>
-                <Links />
-            </motion.div>
-            <ToggleButton setOpen={setOpen}/>
-        </motion.div>
-    )
-}
+  return (
+    <motion.div className="sidebar" animate={open ? "open" : "closed"}>
+      <motion.div className="bg" variants={variants}>
+        <Links />
+      </motion.div>
+      <ToggleButton setOpen={setOpen} />
+    </motion.div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
